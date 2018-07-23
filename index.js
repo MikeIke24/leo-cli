248be2d83af8adb3da5d09076c13b9b385cdf48c
MikeIke24 <mnelms48@gmail.com>
2018-07-23 09:08:23 -0500

    Directory Setup for testing

---
+++ index.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var app_1 = require("./src/app");
+var test = 'test';
+console.log(test);
+console.log(app_1.val);
+//# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,4 +1,17 @@
+#!/usr/bin/env node
 import {val} from "./src/app";
+
 const test = 'test';
 console.log(test);
 console.log(val);
+
+const program = require('commander');
+program
+    .arguments('<file>')
+    .option('-u, --username <username>', 'The user to authenticate as')
+    .option('-p, --password <password>', 'The user\'s password')
+    .action(function (file) {
+        console.log('user: %s pass: %s file: %s',
+            program.username, program.password, file);
+    })
+    .parse(process.argv);
